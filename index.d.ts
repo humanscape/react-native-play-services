ddde32054f7616e32f827492f726e984b3ca9d29
hw0k <hw0knam@gmail.com>
2019-11-14 19:56:58 +0900

    fix: fix type definition error

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,13 @@
+type GooglePlayServicesStatus = {
+  AVAILABLE: 10,
+  GMS_DISABLED: 20,
+  GMS_NEED_UPDATE: 30,
+  INVALID: 30
+};
+
 declare module "react-native-play-services" {
-  const GooglePlayServicesStatus = {
-    AVAILABLE: 10,
-    GMS_DISABLED: 20,
-    GMS_NEED_UPDATE: 21,
-    INVALID: 30
-  } as const;
+  const GooglePlayServicesStatus: GooglePlayServicesStatus;
   const sync: () => Promise<void>;
-  const goToSettings: () => Promise<void>;
+  const goToSetting: () => Promise<void>;
   const goToMarket: () => Promise<void>;
 }
